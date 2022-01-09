26d4fede8f538865849a0cb94e1887317d5bd421
oleksii <oleksii.untila@gmail.com>
2022-01-09 09:29:13 +0000

    join to point

--- elec.php
+++ elec.php
@@ -1,6 +1,9 @@
 <?php
- 
-$elec = ['phone','laptop', 'TV','monitor','vacuum claener'];
-var_dump(implode(",", $elec));
 
+ $elec = ['phone','laptop', 'TV','monitor','vacuum claener'];
 
+$elec = implode(",", $elec);
+
+$elec = str_replace(',', '.', $elec);
+
+var_dump($elec);
